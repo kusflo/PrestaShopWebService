674b2b7ffb120eeab9334d9d4f1142ccac8cbd05
Marcos Redondo <kusflo@gmail.com>
2017-03-26 13:10:21 +0200

    Change to LF end of line

--- src/ServiceSimpleXmlToArray.php
+++ src/ServiceSimpleXmlToArray.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace pshopws;
 
 /**
@@ -9,21 +10,24 @@ class ServiceSimpleXmlToArray
     public static function take($xmlObject)
     {
         $service = new self($xmlObject);
+
         return $service->xmlToArray($xmlObject);
     }
 
     public static function takeMultiple($xmlObjects)
     {
+        $array = array();
         foreach ($xmlObjects as $obj) {
             $array[] = self::take($obj);
         }
+
         return $array;
     }
 
     private function __construct($xmlObject)
     {
         if (!is_a($xmlObject, \SimpleXMLElement::class)) {
-            throw new PShopWsException('The service ' . __CLASS__ . ' has received a parameter other than SimpleXmlElement');
+            throw new PShopWsException('The service '.__CLASS__.' has received a parameter other than SimpleXmlElement');
         }
     }
 
@@ -32,6 +36,7 @@ class ServiceSimpleXmlToArray
         foreach ((array)$xmlObject as $index => $node) {
             $out[$index] = (is_object($node)) ? $this->xmlToArray($node) : $node;
         }
+
         return $out;
     }
 }
